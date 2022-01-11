363bf0fff1d28f4de1d98a6f2e369f0283bb8c03
wakitani <wakitani.kaihei@gmail.com>
2022-01-11 04:09:33 +0000

    Initial commit

---
+++ test/workspaces.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Workspaces from '../lib/workspaces-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/workspaces-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Workspaces.WorkspacesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
