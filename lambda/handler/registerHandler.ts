5d4b8f643a301bd005cf5c1da1d3d1e5c29c50dc
wakitani <wakitani.kaihei@gmail.com>
2022-01-22 05:07:28 +0000

    add validation

--- lambda/handler/registerHandler.ts
+++ lambda/handler/registerHandler.ts
@@ -1,13 +1,81 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from 'aws-lambda';
 import { RegisterDomain, RegisterInfo } from '../domain/register';
+import Ajv from 'ajv';
+
+const ajv = new Ajv();
 
 interface Response {
-    status: string
+    message: string
 }
 
 export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
     console.log('incoming event ', event);
 
+    // bodyがあることを確認
+    if (!event.body) {
+        console.error('body param is not found.');
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'Parameter is invalid.',
+            } as Response),
+        };
+    }
+
+    // bodyのスキーマを定義
+    const bodySchema = {
+        required: ['title', 'description'],
+        type: 'object',
+        properties: {
+            title: {
+                type: 'string',
+            },
+            description: {
+                type: 'string',
+            }
+        }
+    }
+
+    // bodyのスキーマが正しいことを確認
+    let body;
+    try {
+        body = JSON.parse(event.body!)
+    } catch (err) {
+        console.error('JSON parse error');
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'Parameter is invalid.',
+            } as Response),
+        };
+    }
+    console.log('body ', body);
+    const validate = ajv.compile(bodySchema);
+    const valid = validate(body);
+    console.log('valid ', valid);
+
+    if (!valid) {
+        console.error('validation error, ', validate.errors);
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: 'Parameter is invalid.',
+            } as Response),
+        };
+    }
+
+    // もしauthorizationヘッダがなかったら403を返す（ここに到達した時点では確実にあるはずだが一応）
+    if (!event.headers.authorization) {
+        console.error('authorization header is not found.')
+        return {
+            statusCode: 401,
+            body: JSON.stringify({
+                message: 'Unauthorized.',
+            } as Response),
+        };
+    }
+
+    // TODOの登録に必要な情報
     const registerInfo: RegisterInfo = {
         token: event.headers.authorization!,
         title: JSON.parse(event.body!).title,
@@ -20,7 +88,7 @@ export const handler = async (event: APIGatewayProxyEventV2): Promise<APIGateway
     return {
         statusCode: 200,
         body: JSON.stringify({
-            status: 'OK',
+            message: 'OK',
         } as Response),
     };
 };
