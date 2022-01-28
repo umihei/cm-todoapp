95ca6afb07b6aa813b6890563060ac272f15761b
wakitani <wakitani.kaihei@gmail.com>
2022-01-28 23:56:38 +0000

    add update, delete test

--- test/lambda/infra/dynamo/dynamodb-register.test.ts
+++ test/lambda/infra/dynamo/dynamodb-register.test.ts
@@ -4,6 +4,8 @@ process.env.TODO_TABLE_NAME = 'local-todo';
 
 import { RegisterDBInfo } from '../../../../lambda/domain/register';
 import { QueryDBInfo } from '../../../../lambda/domain/query';
+import { UpdateDBInfo } from '../../../../lambda/domain/update';
+import { DeleteDBInfo } from '../../../../lambda/domain/delete';
 import { AccessTodoTable, ddbClient } from '../../../../lambda/infra/accessTodoTable';
 import { DynamoDBClient, PutItemCommand, PutItemCommandInput, PutItemCommandOutput } from '@aws-sdk/client-dynamodb';
 
@@ -52,6 +54,51 @@ describe('todo table service call', (): void => {
 
     });
 
+    test('register ddbClient error', async () => {
+
+        // APIを実行する関数をモック化
+        ddbClient.send = jest.fn().mockRejectedValue({
+            promise: jest.fn().mockRejectedValue('error')
+        });
+
+        // PutItemCommandを実行する関数をモック化
+        AccessTodoTable.executePutItemCommand = jest.fn().mockReturnValue(null);
+
+        // DBへの登録処理を実行する関数へ渡すパラメタ
+        const InputParameter: RegisterDBInfo = {
+            username: 'tarako',
+            title: 'あのこと',
+            description: 'あれやって，これやって'
+        };
+
+        // PutItemCommandへ渡すパラメタの期待値
+        const putItemCommandInputParams = {
+            TableName: 'local-todo',
+            Item: {
+                userName: { S: InputParameter.username },
+                todoId: { S: expect.any(String) },
+                title: { S: InputParameter.title },
+                description: { S: InputParameter.description },
+                lastUpdateDateTime: { S: expect.any(String) },
+            },
+        };
+
+        // DBへの登録処理を実行
+        // const response = await AccessTodoTable.registerNewTodo(InputParameter)
+
+        await expect(AccessTodoTable.registerNewTodo(InputParameter)).rejects.toThrow(new Error('error'))
+
+        // // モック化した関数が１回だけコールされたことをテスト
+        // expect(AccessTodoTable.executePutItemCommand).toHaveBeenCalledTimes(1);
+
+        // // PutItemCommandを呼び出すパラメタが期待通りであるかテスト
+        // expect(AccessTodoTable.executePutItemCommand).toHaveBeenCalledWith(putItemCommandInputParams);
+
+        // // モック化した関数が１回だけコールされたことをテスト
+        // expect(ddbClient.send).toHaveBeenCalledTimes(1);
+
+    });
+
     test('query', async () => {
 
         // APIを実行する関数をモック化
@@ -88,6 +135,100 @@ describe('todo table service call', (): void => {
 
         // PutItemCommandを呼び出すパラメタが期待通りであるかテスト
         expect(AccessTodoTable.executeQueryCommand).toHaveBeenCalledWith(queryCommandInputParams);
+
+        // モック化した関数が１回だけコールされたことをテスト
+        expect(ddbClient.send).toHaveBeenCalledTimes(1);
+
+    });
+
+    test('update', async () => {
+
+        // APIを実行する関数をモック化
+        ddbClient.send = jest.fn().mockReturnValue({
+            promise: jest.fn().mockResolvedValue({
+                Items: [{
+                    test: 'hoge'
+                }]
+            })
+        });
+
+        // UpdateItemCommandを実行する関数をモック化
+        AccessTodoTable.executeUpdateItemCommand = jest.fn().mockReturnValue(null);
+
+        // DBへの更新処理を実行する関数へ渡すパラメタ
+        const inputParameter: UpdateDBInfo = {
+            username: 'tarako',
+            todoid: 'test-todoid',
+            title: 'test-title',
+            description: 'test-description'
+        };
+
+        // updateItemCommandへ渡すパラメタの期待値
+        const updateItemCommandInputParams = {
+            TableName: process.env.TODO_TABLE_NAME,
+            Key: {
+                userName: { S: inputParameter.username },
+                todoId: { S: inputParameter.todoid },
+            },
+            UpdateExpression: "set title = :t, description = :d, lastUpdateDateTime = :l",
+            ExpressionAttributeValues: {
+                ":t": { S: inputParameter.title },
+                ":d": { S: inputParameter.description },
+                ":l": { S: expect.any(String) }
+            }
+        }
+
+        // DBへの更新処理を実行
+        await AccessTodoTable.updateTodo(inputParameter)
+
+        // モック化した関数が１回だけコールされたことをテスト
+        expect(AccessTodoTable.executeUpdateItemCommand).toHaveBeenCalledTimes(1);
+
+        // PutItemCommandを呼び出すパラメタが期待通りであるかテスト
+        expect(AccessTodoTable.executeUpdateItemCommand).toHaveBeenCalledWith(updateItemCommandInputParams);
+
+        // モック化した関数が１回だけコールされたことをテスト
+        expect(ddbClient.send).toHaveBeenCalledTimes(1);
+
+    });
+
+    test('delete', async () => {
+
+        // APIを実行する関数をモック化
+        ddbClient.send = jest.fn().mockReturnValue({
+            promise: jest.fn().mockResolvedValue({
+                Items: [{
+                    test: 'hoge'
+                }]
+            })
+        });
+
+        // UpdateItemCommandを実行する関数をモック化
+        AccessTodoTable.executeDeleteItemCommand = jest.fn().mockReturnValue(null);
+
+        // DBへの更新処理を実行する関数へ渡すパラメタ
+        const inputParameter: DeleteDBInfo = {
+            username: 'tarako',
+            todoid: 'test-todoid'
+        };
+
+        // updateItemCommandへ渡すパラメタの期待値
+        const deleteItemCommandInputParams = {
+            TableName: process.env.TODO_TABLE_NAME,
+            Key: {
+                userName: { S: inputParameter.username },
+                todoId: { S: inputParameter.todoid }
+            }
+        }
+
+        // DBへの更新処理を実行
+        await AccessTodoTable.deleteTodo(inputParameter)
+
+        // モック化した関数が１回だけコールされたことをテスト
+        expect(AccessTodoTable.executeDeleteItemCommand).toHaveBeenCalledTimes(1);
+
+        // PutItemCommandを呼び出すパラメタが期待通りであるかテスト
+        expect(AccessTodoTable.executeDeleteItemCommand).toHaveBeenCalledWith(deleteItemCommandInputParams);
 
         // モック化した関数が１回だけコールされたことをテスト
         expect(ddbClient.send).toHaveBeenCalledTimes(1);
