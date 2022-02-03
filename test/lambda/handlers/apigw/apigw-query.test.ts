b729ea1dbcee111479e51bce18b1a6e31422a739
wakitani <wakitani.kaihei@gmail.com>
2022-02-03 06:22:08 +0000

    fix var type

--- test/lambda/handlers/apigw/apigw-query.test.ts
+++ test/lambda/handlers/apigw/apigw-query.test.ts
@@ -2,7 +2,7 @@ process.env.OS_DOMAIN = 'local'
 process.env.OS_INDEX = 'local'
 const type = '_doc'
 
-import { APIGatewayProxyHandlerV2, APIGatewayProxyEventV2WithJWTAuthorizer, Context } from 'aws-lambda';
+import { APIGatewayProxyEventV2WithJWTAuthorizer, Context } from 'aws-lambda';
 import { handler } from '../../../../lambda/handler/queryHandler';
 import { AccessTodoTable } from '../../../../lambda/infra/accessTodoTable';
 import { AccessOpenSearch } from '../../../../lambda/infra/accessOpenSearch';
@@ -104,7 +104,7 @@ describe('query Input/Output', (): void => {
             awsRequestId: 'test-id'
         } as any;
 
-        const pseudoReturnVal = {
+        const pseudoReturnVal = JSON.stringify({
             "took": 55,
             "timed_out": false,
             "_shards": {
@@ -148,10 +148,7 @@ describe('query Input/Output', (): void => {
                     },
                 ]
             }
-        }
-
-        // // DBにPutする処理をMock化
-        // const queryTodoMock = (AccessTodoTable.queryTodo as jest.Mock).mockResolvedValue(pseudoReturnVal);
+        })
 
         // OpenSearchにクエリする処理をMock化
         const queryTodoMock = (AccessOpenSearch.search as jest.Mock).mockResolvedValue(pseudoReturnVal);
@@ -169,18 +166,16 @@ describe('query Input/Output', (): void => {
             statusCode: 200,
             body: JSON.stringify([
                 {
-                    "lastUpdateDateTime": "2022-02-01T04:58:01.789Z",
-                    "description": "テストテスト",
-                    "title": "test",
                     "todoId": "4b8bdc1d-aa5b-49a4-a537-fab7b8ba6337",
-                    "userName": "tarako"
+                    "title": "test",
+                    "description": "テストテスト",
+                    "lastUpdateDateTime": "2022-02-01T04:58:01.789Z"
                 },
                 {
-                    "lastUpdateDateTime": "2022-02-02T07:18:18.802Z",
-                    "description": "あれをこうする",
-                    "title": "あのこと",
                     "todoId": "93e194b7-e8f0-4301-a7ae-2778bd72b9a8",
-                    "userName": "tarako"
+                    "title": "あのこと",
+                    "description": "あれをこうする",
+                    "lastUpdateDateTime": "2022-02-02T07:18:18.802Z",
                 }
             ]
             ),
